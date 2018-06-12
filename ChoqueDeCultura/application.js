73fbd625eb5d7f388d29c68f8a3b3d33731c6e17
Guilherme Paciulli <guilherme.paciulli@icloud.com>
2018-06-12 17:13:33 -0300

    background image

--- ChoqueDeCultura/application.js
+++ ChoqueDeCultura/application.js
@@ -9,30 +9,40 @@
 //
 
 /*
- * This file provides an example skeletal stub for the server-side implementation 
+ * This file provides an example skeletal stub for the server-side implementation
  * of a TVML application.
  *
- * A javascript file such as this should be provided at the tvBootURL that is 
- * configured in the AppDelegate of the TVML application. Note that  the various 
- * javascript functions here are referenced by name in the AppDelegate. This skeletal 
- * implementation shows the basic entry points that you will want to handle 
+ * A javascript file such as this should be provided at the tvBootURL that is
+ * configured in the AppDelegate of the TVML application. Note that  the various
+ * javascript functions here are referenced by name in the AppDelegate. This skeletal
+ * implementation shows the basic entry points that you will want to handle
  * application lifecycle events.
  */
 
 /**
- * @description The onLaunch callback is invoked after the application JavaScript 
- * has been parsed into a JavaScript context. The handler is passed an object 
+ * @description The onLaunch callback is invoked after the application JavaScript
+ * has been parsed into a JavaScript context. The handler is passed an object
  * that contains options passed in for launch. These options are defined in the
  * swift or objective-c client code. Options can be used to communicate to
- * your JavaScript code that data and as well as state information, like if the 
+ * your JavaScript code that data and as well as state information, like if the
  * the app is being launched in the background.
  *
- * The location attribute is automatically added to the object and represents 
+ * The location attribute is automatically added to the object and represents
  * the URL that was used to retrieve the application JavaScript.
  */
 App.onLaunch = function(options) {
-    var alert = createAlert("Hello World!", "Welcome to tvOS");
-    navigationDocument.pushDocument(alert);
+    evaluateScripts(options.initialJSDependencies,
+      function(success){
+        if (success) {
+          resourceLoader = new ResourceLoaderJS(NativeResourceLoader.create());
+          var initialDoc = resourceLoader.getDocument("main.tvml");
+          navigationDocument.pushDocument(initialDoc);
+        } else {
+          var alert = createAlert("Evaluate Scripts Error", "Error attempting to evaluate the external JS files.");
+          navigationDocument.presentModal(alert);
+          throw ("Playback Example: unable to evaluate scripts.");
+        }
+      });
 }
 
 
@@ -45,21 +55,17 @@ App.onDidEnterBackground = function() {
 }
 
 App.onWillEnterForeground = function() {
-    
+
 }
 
 App.onDidBecomeActive = function() {
-    
+
 }
 
 App.onWillTerminate = function() {
-    
+
 }
 
-
-/**
- * This convenience funnction returns an alert template, which can be used to present errors to the user.
- */
 var createAlert = function(title, description) {
 
     var alertString = `<?xml version="1.0" encoding="UTF-8" ?>
